78131ec2e1fe633a0e80560a24f06acb5fddb47e
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-10 19:57:28 -0500

    update

--- main.c
+++ main.c
@@ -126,7 +126,14 @@ struct node* deleteFirst() {
    //save reference to first link
    struct node *tempLink = head;
 
+   //if only one link
+   if(head->next == NULL){
+      last = NULL;
+   } else {
+      head->next->prev = NULL;
+   }
 
+   head = head->next;
    //return the deleted link
    return tempLink;
 }
